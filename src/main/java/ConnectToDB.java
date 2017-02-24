6de014d1c27bc1a9536704ffa7966f7e15e9678a
malakaganga <malakaganga.acgmail.com>
2017-02-24 16:46:24 +0530

    can add any db

--- src/main/java/ConnectToDB.java
+++ src/main/java/ConnectToDB.java
@@ -6,27 +6,40 @@ import java.sql.*;
 public class ConnectToDB {
 
 
-    public static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-    public static final String JDBC_URL = "jdbc:derby:derbyDB;create=true";
+    public String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    public String JDBC_URL = "jdbc:derby:derbyDB;create=true";
+    public String JDBC_URL_1 = "jdbc:derby:";
+    public String JDBC_URL_2 = ";create=true";
+    public String db_name = "x";
 
-    private static ConnectToDB singleton = new ConnectToDB();
+
 
     private Connection con1;
 
-    private ConnectToDB() {//A private Constructor prevents any other class from instantiating.
-    }
-    public static ConnectToDB getInstance() {//This return the singleton object instance at Class level
-        return singleton;
+    public ConnectToDB(String dbname) {
+        db_name = dbname;
     }
 
-    public Statement accessDataBase() throws ClassNotFoundException, SQLException, IllegalAccessException,
-            InstantiationException {
+    public Statement accessDataBase(){
         Statement statement = null;
 
+        try {
+            JDBC_URL = JDBC_URL_1+db_name+JDBC_URL_2;
             Class.forName(DRIVER).newInstance();
             Connection con = DriverManager.getConnection(JDBC_URL);//Connect to the database
             con1 = con;
             statement = con.createStatement();
+
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
         return statement;
 
--- src/main/java/DerbyApp.java
+++ src/main/java/DerbyApp.java
@@ -1,11 +1,9 @@
-import java.sql.SQLException;
-
 /**
  * Created by Jayz on 23/02/2017.
  */
 public class DerbyApp {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, IllegalAccessException,
-            InstantiationException {
+    public static void main(String[] args){
+
     }
 
 }
