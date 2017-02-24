b13a95976098dd60ccea39f537d09965ae4558bb
malakaganga <malakaganga.acgmail.com>
2017-02-24 15:55:05 +0530

    added the necessary

--- src/main/java/DerbyApp.java
+++ src/main/java/DerbyApp.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DerbyApp {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, IllegalAccessException,
             InstantiationException {
-        QuearyDB qdb = new QuearyDB();
+        System.out.println("ndbsbdmsbdn");
     }
 
 }
